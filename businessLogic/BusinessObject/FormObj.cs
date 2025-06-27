88ab11542babbe2c71e18bff6ff35c15e7375293
Pushpak90 <pushpakafasate90@gmail.com>
2025-06-27 23:06:34 +0530

    ADO API

---
+++ businessLogic/BusinessObject/FormObj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace businessLogic.BusinessObject
+{
+    public class FormObj
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string college { get; set; }
+    }
+}
